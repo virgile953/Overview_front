1948f7d2e1fa9ddd3305d5960a9568045aa7effd
Virgile <virgile.barbera@gmail.com>
2025-09-25 01:43:46 +0200

    feat: remove device fetching from log component to streamline data retrieval

--- app/dashboard/logs/page.tsx
+++ app/dashboard/logs/page.tsx
@@ -1,17 +1,13 @@
-import { getDevices } from "@/models/server/devices";
 import { getLogCount, getLogsForChart } from "@/models/server/logs";
 import LogChart from "./chart";
 
 export default async function Logs() {
-  const [count, devices, chartData] = await Promise.all([
+  const [count, chartData] = await Promise.all([
     getLogCount(),
-    getDevices(),
     getLogsForChart(new Date(), new Date(), 24, "hour"),
   ]);
 
   console.log('Chart data:', chartData);
-
-  const devicesId = new Map(devices.map(device => [device.$id, device.name]));
 
   return (
     <div className="space-y-6">
