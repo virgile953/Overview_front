dd6a685e95588385e4d93de7eb777fda8260b9e9
Virgile <virgile.barbera@gmail.com>
2025-11-01 20:26:45 +0100

    refactor: remove orgId parameter from UsersComponent and update usage in Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,7 +21,7 @@ export default async function Dashboard() {
       <div className="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-4 mt-4">
         <DevicesDashboard orgId={session.session.activeOrganizationId} className="col-span-4 md:col-span-4 lg:col-span-5" />
         <GroupsComponent className="col-span-2 md:col-span-2 lg:col-span-2" />
-        <UsersComponent orgId={session.session.activeOrganizationId} className="col-span-2 md:col-span-2 lg:col-span-3" />
+        <UsersComponent className="col-span-2 md:col-span-2 lg:col-span-3" />
         <GroupsComponent className="col-span-2 md:col-span-2 lg:col-span-2" />
         <DevicesDashboard orgId={session.session.activeOrganizationId} className="col-span-4 md:col-span-4 lg:col-span-5" />
         <GroupsComponent className="col-span-2 md:col-span-2 lg:col-span-2" />
